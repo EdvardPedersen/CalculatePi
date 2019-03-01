47c06bdb468092abac9b134016545c7e18869c10
Edvard Pedersen <edvard.pedersen@uit.no>
2019-03-01 22:42:44 +0100

    add more comments and restructure code to make it easier to use

--- get_pi.py
+++ get_pi.py
@@ -1,3 +1,9 @@
+"""
+This application calculates pi using a monte carlo method,
+and visualizes the results compared to the constant in the
+math library
+"""
+
 from random import random
 from math import sqrt
 from math import pi
@@ -7,58 +13,104 @@ from time import clock
 import pygame
 
 def calculate_pi(iterations):
+    """
+    Calculates pi using a monte carlo method, returns pi
+
+    As the are of a circle is pi * r ^ 2, and the area of
+    a square is (2 * r) ^ 2, we can calculate pi by estimating
+    the ratio between points inside a circle and a square where
+    the circle fits perfectly inside the square.
+    When this ratio is multiplied by 4, the result is pi.
+
+    Arguments:
+    iterations -- the number of random points generated
+    """
+    # TODO: Replace with an iterator?
     points_inside = 0
-    points_outside = 0
     for _ in range(iterations):
-        x = random() - 0.5
-        y = random() - 0.5
-        if sqrt(x**2 + y**2) > 0.5:
-            points_outside += 1
-        else:
+        # Generate a random point between (-0.5, -0.5) and (0.5, 0.5)
+        x_position = random() - 0.5
+        y_position = random() - 0.5
+        # Check if point is inside the circle
+        if sqrt(x_position**2 + y_position**2) <= 0.5:
             points_inside += 1
-    total_points = points_outside + points_inside
-    estimated_pi = 4 * points_inside / total_points
+    estimated_pi = 4 * points_inside / iterations
     return estimated_pi
 
-# Pygame setup
-pygame.init()
-screen = pygame.display.set_mode((800, 800))
 
-# Variables used in loop
-current_iterations = 100
-accumulated_error = 0
-values = deque()
 
-# Setup points
-point = pygame.Surface((1, 5))
-point.fill((0, 255, 0))
+class PiEstimation:
+    """
+    Class for visualizing and calculating pi
+    """
+    def __init__(self):
+        """
+        Sets up pygame, and variables used for visualization
+        DO NOT MODIFY
+        """
+        # Pygame initialization
+        pygame.init()
+        self.screen = pygame.display.set_mode((800, 800))
+        self.point = pygame.Surface((1, 5))
+        self.point.fill((0, 255, 0))
 
-# Setup timer
-time_iteration = clock()
+        # Variables used for visualization
+        self.accumulated_error = 0
+        self.values = deque(maxlen=800)
+        self.time_iteration = clock()
 
-# Get y value based on pi value
-def pi2y(value):
-    return int((value - 2.5) * 600)
+        # Calculated pi for the current number of iterations
+        self.current_iterations = 100
+        self.current_pi = 0
 
-while True:
-    # Calculate pi
-    current_pi = calculate_pi(current_iterations)
-    current_iterations += 1
+    def mainloop(self):
+        '''
+        Continously calculates a more correct value for pi
+        and draws this on screen, loops forever.
+        '''
+        # TODO: Change to use an iterator?
+        while True:
+            self.current_pi = calculate_pi(self.current_iterations)
+            self.current_iterations += 1
+            self._draw_screen()
 
-    screen.fill((0, 0, 0))
-    current_error = abs(current_pi - pi)
-    current_y = pi2y(current_pi)
-    values.append(current_y)
-    if len(values) > 799:
-        values.popleft()
-    for x, y in enumerate(values):
-        screen.blit(point, (x, y))
-    pygame.draw.line(screen, (255, 0, 0), (750, pi2y(pi)), (800, pi2y(pi)), 10)
-    pygame.display.flip()
-    accumulated_error += current_error
-    if current_iterations % 100 == 0:
-        time_passed = clock() - time_iteration
-        output = "At iteration {:>6}, average error is {:<8.5}, done in {:<4.2} seconds ({} FPS)"
-        print(output.format(current_iterations, accumulated_error/100, time_passed, int(100/time_passed)))
-        accumulated_error = 0
-        time_iteration = clock()
+    def _draw_screen(self):
+        """
+        Draws the last 800 estimated values for pi, as well as
+        the reference from math.pi
+        DO NOT MODIFY
+        """
+        # Clear screen
+        self.screen.fill((0, 0, 0))
+        self.accumulated_error += abs(self.current_pi - pi)
+
+        # Add most current 
+        self.values.append(self._pi2y(self.current_pi))
+
+        # Draw all estimates on the screen
+        for x_position, y_position in enumerate(self.values):
+            self.screen.blit(self.point, (x_position, y_position))
+
+        # Draw the reference pi value on the screen
+        pygame.draw.line(self.screen, (255, 0, 0), (750, self._pi2y(pi)), (800, self._pi2y(pi)), 3)
+        pygame.display.flip()
+
+        # Print statistics every 100 iterations
+        if self.current_iterations % 100 == 0:
+            time_passed = clock() - self.time_iteration
+            output = "At iteration {:>6}, average error is {:<8.5}, " \
+                     "done in {:<4.2} seconds ({} FPS)"
+            print(output.format(self.current_iterations,
+                                self.accumulated_error/100,
+                                time_passed,
+                                int(100/time_passed)))
+            self.accumulated_error = 0
+            self.time_iteration = clock()
+
+    def _pi2y(self, value):
+        # Do not modify this method
+        return int((value - 2.5) * 600)
+
+if __name__ == "__main__":
+    estimator = PiEstimation()
+    estimator.mainloop()
